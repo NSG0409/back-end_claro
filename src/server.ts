d547140eee7ff612b574d71a2e7dba935084af95
Natan Guedes <nathan.guedes.4@gmail.com>
2021-01-12 23:05:09 -0300

    Rota Post criada

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.post('/form', (request, response) => {
+    console.log(request.body);
+    response.send('caralho do caralho');
+})
 
 app.listen(3333);
