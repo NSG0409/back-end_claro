9f411ee39369b02c35372d426afbdee01fcea3fe
Natan Guedes <nathan.guedes.4@gmail.com>
2021-01-20 20:06:14 -0300

    Configurando o Firebase

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use(cors());
+
 app.post('/form', (request, response) => {
     console.log(request.body);
-    response.send('caralho do caralho');
+    //db.collection('clients').add(data) -> Firebase
+    response.send('Rodando...');
 })
 
 app.listen(3333);
